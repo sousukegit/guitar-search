61fc8c57c36d0c02c49baf1ce314b35baf110914
sousukegit <greenneosoul.sousuke@gmail.com>
2025-02-15 10:53:28 +0900

    メインページにクエリからコンテンツを表示することに成功

--- app/api/songs/route.ts
+++ app/api/songs/route.ts
@@ -3,7 +3,12 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET() {
   try {
-    const songs = await prisma.song.findMany(); // `song` テーブルからデータ取得
+    const songs = await prisma.song.findMany({
+      include: {
+        artist: true, // `artist` テーブルと紐づけ
+        soro: true,
+      },
+    }); // `song` テーブルからデータ取得
     return NextResponse.json(songs);
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch songs" }, { status: 500 });
